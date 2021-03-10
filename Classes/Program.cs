b348522af8dd4ae9a5d26b8d4479ed8ec8f7de64
ACER <ACER@DESKTOP-TRKNRSV>
2021-03-10 11:18:49 +0300

    Add project files.

---
+++ Classes/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add();
+            customerManager.Update();
+
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add();
+            productManager.Update();
+
+
+            Customer customer = new Customer();
+            customer.Id = 33;
+            customer.FirstName = "Türkan";
+            customer.LastName = "Rişvan";
+            customer.City = "Mersin";
+
+            Customer customer1 = new Customer() { Id = 22, FirstName = "Gökhan", LastName = "Rişvan", City = "Antalya" };
+
+            Console.WriteLine(customer1.FirstName);
+
+        }
+    }
+}
+
